42f38a8dbb541d252fc12b726605bc677d9625fe
Charlie Otero-Davenport <cdavenport090@gmail.com>
2025-12-19 18:02:45 -0500

    mostly html and css changes idk im justt commiting

--- src/app/software-dev-tab-page/software-dev-tab-page.component.ts
+++ src/app/software-dev-tab-page/software-dev-tab-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, ChangeDetectionStrategy, signal } from "@angular/core";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { MatDividerModule } from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 @Component({
     selector: 'software-dev-tab-page',
-    imports: [MatExpansionModule, MatDividerModule],
+    imports: [MatExpansionModule, MatDividerModule, MatListModule],
     templateUrl: './software-dev-tab-page.template.html',
     changeDetection: ChangeDetectionStrategy.OnPush
 })
